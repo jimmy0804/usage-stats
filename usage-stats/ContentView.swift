585f2d29f017ce7e726c87a9d252e6d522b49574
Jimmy Yeung <jiyeung@ebay.com>
2020-04-03 14:49:41 +0800

    Initial Commit

---
+++ usage-stats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  usage-stats
+//
+//  Created by Yeung, Jimmy(AWF) on 3/4/2020.
+//  Copyright © 2020 Jimmy Yeung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
